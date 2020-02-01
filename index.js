fb2287be48912644cd45cb06631048eab20fe55a
nuggetsnegin <nuggetsnegin@gmail.com>
2020-01-31 20:34:56 -0500

    swapping array values to ensure they're always unique

--- index.js
+++ index.js
@@ -1,65 +1,17 @@
-// Global emoji map?:
-// Each number is shown as a different asset, but behind the scenes is just a number.
-const emojiMap = {
-	1: "fish",
-	2: "wetFood",
-	3: "yarn"
-};
-/* Game Class:
-    Keeps track of timer
-    Keeps track of score?
-    Ability to start, end game
-*/
+const progress = 0
+const currentCombo = []
+const correctCombo = [1,2,3]
 
-class Game {
-	progress = 0; // Progress bar values, will fill based on value.
-	constructor(progress) {
-		this.progress = progress;
-	}
-	// Start game (i.e start press, or restart)
-	start() {
-		const roboCat = Cat();
-	}
-	// When game ends, stop timer, etc.
-	end() {}
-	// pause(){} // Stretch goal, ability to pause game with a button/'ESC'
+/*swapping around the array only the unique values*/
+generateCombo = combo => {
+  for (let i = combo.length - 1; i > 0; i--) {
+	let j = Math.floor(Math.random() * (i + 1))
+	console.log(j);
+    let temp = combo[i]
+    combo[i] = combo[j]
+	combo[j] = temp
+  }
+  return combo;
 }
 
-/* Cat Class:
-    Keeps track of combinations
-    Number of successful combos
-*/
-class Cat {
-	// We keep track of how many key strokes we are at (3 max), and also how many correct (inversely how many incorrect)
-	// I.e if current: 2 and correct: 0, we know we are 2 incorrect.
-	keystrokes = {
-		current: 0,
-		correct: 0
-	};
-	totalScore = 0;
-
-	constructor(combination, keystrokes, totalScore) {
-		this.combination = combination;
-		this.keystrokes = keystrokes;
-		this.totalScore = totalScore;
-	}
-	// Getter for combination
-	get combination() {
-		return this.combination;
-	}
-	// Generate a combination
-	generateCombination() {
-		return [1, 2, 3]; // Randomize a 3 digit combination of 1, 2, 3
-	}
-	calculateStreak(keystrokes) {}
-	checkCombination() {
-		/*
-            When the User hits 3 keys/a combination, we "submit" the combination and generate a new combination
-            We also add/decrease progress based on total combination (decrement if 3 wrong)
-        */
-	}
-}
-
-// Game loop:
-const game = Game();
-game.start();
+console.log(generateCombo(correctCombo));
