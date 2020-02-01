dfbfac2fe0107276a2a3afd001d1c0f25d961cdf
nuggetsnegin <nuggetsnegin@gmail.com>
2020-01-31 20:50:07 -0500

    i forgot

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const progress = 0
 const currentCombo = []
 const correctCombo = [1,2,3]
 
+
+
 /*swapping around the array only the unique values*/
 generateCombo = combo => {
   for (let i = combo.length - 1; i > 0; i--) {
 	let j = Math.floor(Math.random() * (i + 1))
-	console.log(j);
     let temp = combo[i]
     combo[i] = combo[j]
 	combo[j] = temp
   }
   return combo;
+}
+
+
+checkComboButton = ()=>{
+	
 }
 
 console.log(generateCombo(correctCombo));
