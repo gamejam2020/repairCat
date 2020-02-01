cefa02299b315e54dc142e9c823530d286b57c1d
Andrew Jung <andrewdhjung@gmail.com>
2020-01-31 19:59:09 -0500

    pivot game classes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Global emoji map?:
 // Each number is shown as a different asset, but behind the scenes is just a number.
 const emojiMap = {
-	1: "",
-	2: "",
-	3: ""
+	1: "fish",
+	2: "wetFood",
+	3: "yarn"
 };
 /* Game Class:
     Keeps track of timer
@@ -12,26 +12,36 @@ const emojiMap = {
 */
 
 class Game {
-	timer = 60; // Default, 60 seconds?
-	constructor(timer) {
-		this.timer = timer;
+	progress = 0; // Progress bar values, will fill based on value.
+	constructor(progress) {
+		this.progress = progress;
 	}
 	// Start game (i.e start press, or restart)
-	start() {}
+	start() {
+		const roboCat = Cat();
+	}
 	// When game ends, stop timer, etc.
 	end() {}
 	// pause(){} // Stretch goal, ability to pause game with a button/'ESC'
 }
+
 /* Cat Class:
     Keeps track of combinations
     Number of successful combos
 */
-
 class Cat {
+	// We keep track of how many key strokes we are at (3 max), and also how many correct (inversely how many incorrect)
+	// I.e if current: 2 and correct: 0, we know we are 2 incorrect.
+	keystrokes = {
+		current: 0,
+		correct: 0
+	};
 	totalScore = 0;
-	constructor(combination, totalScore) {
-		this.totalScore = totalScore;
+
+	constructor(combination, keystrokes, totalScore) {
 		this.combination = combination;
+		this.keystrokes = keystrokes;
+		this.totalScore = totalScore;
 	}
 	// Getter for combination
 	get combination() {
@@ -39,7 +49,14 @@ class Cat {
 	}
 	// Generate a combination
 	generateCombination() {
-		return [1, 2, 3, 4, 5]; // Randomize a 5 unique array, placeholder
+		return [1, 2, 3]; // Randomize a 3 digit combination of 1, 2, 3
+	}
+	calculateStreak(keystrokes) {}
+	checkCombination() {
+		/*
+            When the User hits 3 keys/a combination, we "submit" the combination and generate a new combination
+            We also add/decrease progress based on total combination (decrement if 3 wrong)
+        */
 	}
 }
 
